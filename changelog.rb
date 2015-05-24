e0df085acd1d8ed0298e5d2b1f3f345cf96b16e2
MeXx <mexx@devsub.net>
2015-05-24 23:28:31 +0200

    feat(logging): initialize logger

--- changelog.rb
+++ changelog.rb
@@ -1,4 +1,7 @@
-#!/usr/bin/env rubyrequire "docopt"
+#!/usr/bin/env ruby
+require "docopt"
+require "logger"
+require "pry"
 
 doc = <<DOCOPT
 A script to generate release-notes from a git repository
@@ -9,20 +12,24 @@ Entries for the release notes must have a special format:
 `* feat: <description>`
 
 Usage:
-  #{__FILE__} changelog
-  #{__FILE__} changelog <from-commit>
-  #{__FILE__} changelog <from-commit>..<to-commit>
+  #{__FILE__} [--debug]
+  #{__FILE__} <from-commit> [--debug]
+  #{__FILE__} <from-commit> <to-commit> [--debug]
   #{__FILE__} -h | --help
   #{__FILE__} --version
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
+  --debug   Show debug output
 DOCOPT
 
 begin
-  require "pp"
-  pp Docopt::docopt(doc)
+  args =  Docopt::docopt(doc, version: '0.0.1')
+  logger = Logger.new(STDOUT)
+  logger.level = args["--debug"] ?  Logger::DEBUG : Logger::ERROR
+  
 rescue Docopt::Exit => e
   puts e.message
 end
+
