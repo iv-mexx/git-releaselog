a7e7cf497a524af198cd1c07aaee66742ca90692
MeXx <mexx@devsub.net>
2015-05-24 22:42:18 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.2.0'
+# ruby-gemset=rails
+
+gem 'rugged'
+gem 'docopt'
---
+++ release-notes.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# A script to generate release-notes from a git repository
+# 
+# Entries for the release notes must have a special format:
+# 
+# `* fix: <description>`
+# `* feat: <description>`
+#
+
+
