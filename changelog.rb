a7e9569d85ce941cd98653ee40ceafdfb46faf0e
MeXx <mexx@devsub.net>
2015-05-25 14:22:12 +0200

    feat(printing): printing a nice change log done

--- changelog.rb
+++ changelog.rb
@@ -15,20 +15,21 @@ Entries for the release notes must have a special format:
 `* feat: <description>`
 
 Usage:
-  #{__FILE__} [--debug]
-  #{__FILE__} <from-commit> [--debug]
-  #{__FILE__} <from-commit> <to-commit> [--debug]
-  #{__FILE__} -h | --help
-  #{__FILE__} --version
+#{__FILE__} [--debug]
+#{__FILE__} <from-commit> [--debug]
+#{__FILE__} <from-commit> <to-commit> [--debug]
+#{__FILE__} -h | --help
+#{__FILE__} --version
 
 Options:
-  from-commit   From which commit should the log be followed? Will default to head
-  to-commit     To which commit should the log be followed? Will default to the latest tag
-  -h --help     Show this screen.
-  --version     Show version.
-  --debug       Show debug output
+from-commit   From which commit should the log be followed? Will default to head
+to-commit     To which commit should the log be followed? Will default to the latest tag
+-h --help     Show this screen.
+--version     Show version.
+--debug       Show debug output
 DOCOPT
 
+# Parse Commandline Arguments
 begin
   args =  Docopt::docopt(doc, version: '0.0.1')
 rescue Docopt::Exit => e
@@ -36,6 +37,7 @@ rescue Docopt::Exit => e
   exit
 end
 
+# Initialize Logger
 logger = Logger.new(STDOUT)
 logger.level = args["--debug"] ?  Logger::DEBUG : Logger::ERROR
 
@@ -48,13 +50,34 @@ rescue Rugged::OSError => e
   exit
 end
 
+arg_from = args["<from-commit>"]
+arg_to = args["<to-commit>"]
+
+# Find if we're operating on tags
+tag_from = tagWithName(repo, arg_from)
+tag_to = tagWithName(repo, arg_to)
+tag_latest = latestTagID(repo, logger)
+
+if tag_from
+  logger.info("Found Tag #{tag_from.name} to start from")
+end
+
+if tag_to
+  logger.info("Found Tag #{tag_to.name} to end at")
+end
+
+if tag_latest
+  logger.info("Latest Tag found: #{tag_latest.name}")
+end
+
 
 # From which commit should the log be followed? Will default to head
-commit_from = commit(repo, args["<from-commit>"], logger) || repo.head.target
+commit_from = (tag_from && tag_from.target) || commit(repo, arg_from, logger) || repo.head.target
 
 # To which commit should the log be followed? Will default to the latest tag
-commit_to = commit(repo, args["<to-commit>"], logger) || latestTagID(repo, logger)
+commit_to = (tag_to && tag_to.target) || commit(repo, arg_to, logger) || tag_latest && (tag_latest.target)
 
+logger.info("Traversing git tree from commit #{commit_from.oid} to commit #{commit_to && commit_to.oid}")
 
 # Initialize a walker that walks through the commits from the <from-commit> to the <to-commit>
 walker = Rugged::Walker.new(repo)
@@ -62,8 +85,13 @@ walker.sorting(Rugged::SORT_DATE)
 walker.push(commit_from)
 walker.hide(commit_to.parents.first) unless commit_to == nil
 
-# Parse all commits
+# Parse all commits and extract changes
 changes = walker.map{ |c| parseCommit(c)}.reduce(:+)
 
 logger.info("Found #{changes.count} changes")
 
+# Create the changelog
+log = Changelog.new(changes, tag_from, tag_to || tag_latest, commit_from, commit_to)
+
+# Print the changelog
+puts log.to_slack
--- changelog_helpers.rb
+++ changelog_helpers.rb
@@ -2,6 +2,66 @@
 # Helper Functions for git-changelog script
 # 
 
+# A class for representing a change
+# A change can have a type (fix or feature) and a note describing the change
+class Change
+  FIX = 1
+  FEAT = 2
+
+  TOKEN_FIX = "* fix: "
+  TOKEN_FEAT = "* feat: "
+
+  def initialize(type, note)
+    @type = type
+    @note = note
+  end
+
+  def type
+    @type
+  end
+
+  def note
+    @note
+  end
+
+  def self.parse(line)
+    if line.start_with? Change::TOKEN_FEAT
+      self.new(Change::FEAT, line.split(Change::TOKEN_FEAT).last)
+    elsif line.start_with? Change::TOKEN_FIX
+      self.new(Change::FIX, line.split(Change::TOKEN_FIX).last)
+    else
+      nil
+    end
+  end
+end
+
+# A class for representing a changelog consisting of several changes
+# over a certain timespan (between two commits)
+class Changelog
+  def initialize(changes, tag_from = nil, tag_to = nil, from_commit = nil, to_commit = nil)
+    @fixes = changes.select{ |c| c.type == Change::FIX }
+    @features = changes.select{ |c| c.type == Change::FEAT }
+    @tag_from = tag_from
+    @tag_to = tag_to
+    @commit_from = from_commit
+    @commit_to = to_commit
+  end
+
+  def since
+    "#{@tag_to && @tag_to.name || @commit_to.object_id} (#{@commit_to && @commit_to.time.strftime("%d.%m.%Y")})"
+  end
+
+  def to_slack
+    str = "Changes since #{since}\n"
+    str << "*Fixes*\n"
+    str << @fixes.map{|c| "\t- #{c.note}"}.join("\n")
+    str << "\n\n*Features*\n"
+    str << @features.map{|c| "\t- #{c.note}"}.join("\n")
+    str << "\n"
+    str    
+  end
+end
+
 # check if the given refString (tag name or commit-hash) exists in the repo 
 def commit(repo, refString, logger)
   return unless refString != nil
@@ -22,37 +82,13 @@ end
 def latestTagID(repo, logger)
   return nil unless repo.tags.count > 0
   sorted_tags = repo.tags.sort { |t1, t2| t1.target.time <=> t2.target.time }
-  sorted_tags.last.target
+  sorted_tags.last
 end
 
-
-# module Change
-#   FIX = 1
-#   FEAT = 2
-# end
-
-# A class for representing a change
-class Change
-  FIX = 1
-  FEAT = 2
-
-  TOKEN_FIX = "* fix: "
-  TOKEN_FEAT = "* feat: "
-
-  def initialize(type, note)
-    @type = type
-    @note = note
-  end
-
-  def self.parse(line)
-    if line.start_with? Change::TOKEN_FEAT
-      self.new(Change::FEAT, line.split(Change::TOKEN_FEAT).last)
-    elsif line.start_with? Change::TOKEN_FIX
-      self.new(Change::FIX, line.split(Change::TOKEN_FIX).last)
-    else
-      nil
-    end
-  end
+# Returns the tag with the given name (if exists)
+def tagWithName(repo, name)
+  tags = repo.tags.select { |t| t.name == name }
+  return tags.first unless tags.count < 1
 end
 
 # Parses a commit message and returns an array of Changes
