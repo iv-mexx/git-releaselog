b597430fb8400c7af0edb7a34d354d6ad916f78e
Markus Chmelar <mexx@devsub.net>
2015-10-04 15:34:23 +0200

    Version Bump by fastlane

--- lib/git-releaselog/version.rb
+++ lib/git-releaselog/version.rb
@@ -1,5 +1,5 @@
 module Releaselog
-    VERSION     = "0.6.1"
+    VERSION     = "0.6.2"
     SUMMARY     = "Generate a releaselog from a git repository"
     DESCRIPTION = "Write your releaselog as part of your usual commit messages. This tool generates a useful releaselog from marked lines in your git commit messages"
 end
