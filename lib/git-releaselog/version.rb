fde53e6b6d156a1dba698a73e285508799373c22
Markus Chmelar <mexx@devsub.net>
2015-10-04 17:26:38 +0200

    chore(appVersion): reset the app version to what it was

--- lib/git-releaselog/version.rb
+++ lib/git-releaselog/version.rb
@@ -1,5 +1,5 @@
 module Releaselog
-    VERSION     = "0.6.2"
+    VERSION     = "0.6.1"
     SUMMARY     = "Generate a releaselog from a git repository"
     DESCRIPTION = "Write your releaselog as part of your usual commit messages. This tool generates a useful releaselog from marked lines in your git commit messages"
 end
