6b7604f86ea4aad4a475a4239aeb4d2e3ae95f1c
Markus Chmelar <mexx@devsub.net>
2015-10-09 15:25:10 +0200

    test(releaselog): add specs for output format options

--- spec/releaselog_spec.rb
+++ spec/releaselog_spec.rb
@@ -4,20 +4,21 @@ require 'git-releaselog'
 include Releaselog
 
 describe Releaselog do
+  subject do
+    Releaselog::Releaselog.generate_releaselog(
+      repo_path: ".",
+      from_ref: arguments["<from-ref>"],
+      to_ref: arguments["<to-ref>"],
+      scope: arguments["--scope"],
+      format: arguments["--format"] || "slack",
+      generate_complete: arguments["--complete"],
+      verbose: (arguments["--debug"] ? true : false)
+      )
+  end
+
   describe "Calling Releaselog" do
     context "without parameters" do
       let(:arguments) { arguments = {} }
-      subject do
-        Releaselog::Releaselog.generate_releaselog(
-          repo_path: ".",
-          from_ref: arguments["<from-ref>"],
-          to_ref: arguments["<to-ref>"],
-          scope: arguments["--scope"],
-          format: arguments["--format"] || "slack",
-          generate_complete: arguments["--complete"],
-          verbose: (arguments["--debug"] ? true : false)
-          )
-      end
 
       it "should create some output labelled as 'Unreleased'" do
         expect(subject).to include("Unreleased")
@@ -26,17 +27,6 @@ describe Releaselog do
 
     context "from a certain tag" do 
       let(:arguments) { arguments = {"<from-ref>" => "0.5.0"} }
-      subject do
-        Releaselog::Releaselog.generate_releaselog(
-          repo_path: ".",
-          from_ref: arguments["<from-ref>"],
-          to_ref: arguments["<to-ref>"],
-          scope: arguments["--scope"],
-          format: arguments["--format"] || "slack",
-          generate_complete: arguments["--complete"],
-          verbose: (arguments["--debug"] ? true : false)
-          )
-      end
 
       it "should create some output labelled as 'Unreleased'" do
         expect(subject).to include("Unreleased")
@@ -52,17 +42,6 @@ describe Releaselog do
 
     context "from one tag to another tag" do
       let(:arguments) { arguments = {"<from-ref>" => "0.5.0", "<to-ref>" => "0.6.0"} }
-      subject do
-        Releaselog::Releaselog.generate_releaselog(
-          repo_path: ".",
-          from_ref: arguments["<from-ref>"],
-          to_ref: arguments["<to-ref>"],
-          scope: arguments["--scope"],
-          format: arguments["--format"] || "slack",
-          generate_complete: arguments["--complete"],
-          verbose: (arguments["--debug"] ? true : false)
-          )
-      end
 
       it "should create some output labelled as '0.6.0' and the date of 0.6.0" do
         expect(subject).to include("0.6.0")
@@ -91,17 +70,6 @@ describe Releaselog do
 
     context "creating complete changelog" do
       let(:arguments) { arguments = {"--complete" => true } }
-      subject do
-        Releaselog::Releaselog.generate_releaselog(
-          repo_path: ".",
-          from_ref: arguments["<from-ref>"],
-          to_ref: arguments["<to-ref>"],
-          scope: arguments["--scope"],
-          format: arguments["--format"] || "slack",
-          generate_complete: arguments["--complete"],
-          verbose: (arguments["--debug"] ? true : false)
-          )
-      end
 
       it "should do contain known release tags" do
         expect(subject).to include("0.1.0")
@@ -124,17 +92,6 @@ describe Releaselog do
 
       describe "no scope" do 
         let(:arguments) { arguments = {"<from-ref>" => from_commit, "<to-ref>" => to_commit} }
-        subject do
-          Releaselog::Releaselog.generate_releaselog(
-            repo_path: ".",
-            from_ref: arguments["<from-ref>"],
-            to_ref: arguments["<to-ref>"],
-            scope: arguments["--scope"],
-            format: arguments["--format"] || "slack",
-            generate_complete: arguments["--complete"],
-            verbose: (arguments["--debug"] ? true : false)
-            )
-        end
 
         it "should include the correct entries" do
           expect(subject).to include("this is just a test changelog entry without scope to be able to test scopes")
@@ -150,17 +107,6 @@ describe Releaselog do
 
       describe "`testscope1`" do
         let(:arguments) { arguments = {"<from-ref>" => from_commit, "<to-ref>" => to_commit, "--scope" => "testscope1" } }
-        subject do
-          Releaselog::Releaselog.generate_releaselog(
-            repo_path: ".",
-            from_ref: arguments["<from-ref>"],
-            to_ref: arguments["<to-ref>"],
-            scope: arguments["--scope"],
-            format: arguments["--format"] || "slack",
-            generate_complete: arguments["--complete"],
-            verbose: (arguments["--debug"] ? true : false)
-            )
-        end
 
         it "should include the correct entries" do
           expect(subject).to include("this is just a test changelog entry without scope to be able to test scopes")
@@ -176,17 +122,6 @@ describe Releaselog do
 
       describe "`testscope2`" do
         let(:arguments) { arguments = {"<from-ref>" => from_commit, "<to-ref>" => to_commit, "--scope" => "testscope2" } }
-        subject do
-          Releaselog::Releaselog.generate_releaselog(
-            repo_path: ".",
-            from_ref: arguments["<from-ref>"],
-            to_ref: arguments["<to-ref>"],
-            scope: arguments["--scope"],
-            format: arguments["--format"] || "slack",
-            generate_complete: arguments["--complete"],
-            verbose: (arguments["--debug"] ? true : false)
-            )
-        end
 
         it "should include the correct entries" do
           expect(subject).to include("this is just a test changelog entry without scope to be able to test scopes")
@@ -197,6 +132,41 @@ describe Releaselog do
         it "should not include the scope tags anymore" do
           expect(subject).not_to include("[testscope1]")
           expect(subject).not_to include("[testscope2]")
+        end
+      end
+    end
+
+    context "output formats" do 
+      let(:from_commit) { "932dc90"}
+      let(:to_commit) { "f036a8b"}
+
+      describe "slack" do
+        let(:arguments) { arguments = {"<from-ref>" => from_commit, "<to-ref>" => to_commit, "--format" => "slack"} }
+
+        it "should produce output formatted for slack" do
+          expect(subject).to include("*Refactor*")
+          expect(subject).to include("\t- this is just a test changelog entry without scope to be able to test scopes\n")
+          expect(subject).to include("\t- [testscope1] this is just a test changelog entry for scope `testscope1` to be able to test scopes\n")
+          expect(subject).to include("\t- [testscope2] this is just a test changelog entry for scope `testscope2` to be able to test scopes\n")
+        end
+      end
+
+      describe "markdown" do
+        let(:arguments) { arguments = {"<from-ref>" => from_commit, "<to-ref>" => to_commit, "--format" => "md"} }
+
+        it "should produce output formatted for slack" do
+          expect(subject).to include("#### Refactor")
+          expect(subject).to include("* this is just a test changelog entry without scope to be able to test scopes\n")
+          expect(subject).to include("* [testscope1] this is just a test changelog entry for scope `testscope1` to be able to test scopes\n")
+          expect(subject).to include("* [testscope2] this is just a test changelog entry for scope `testscope2` to be able to test scopes\n")
+        end
+      end
+
+      describe "raw" do
+        let(:arguments) { arguments = {"<from-ref>" => from_commit, "<to-ref>" => to_commit, "--format" => "raw"} }
+
+        it "should produce output formatted for slack" do
+          expect(subject).to be_a(Changelog)
         end
       end
     end
